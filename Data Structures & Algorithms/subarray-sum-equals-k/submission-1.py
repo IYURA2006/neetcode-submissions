726b8eb1874c828add4fe30334f94edd7c8d3b6e
Yurii Ilnytskyi <49524463+IYURA2006@users.noreply.github.com>
2026-07-02 18:53:16 +0100

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,28 @@
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        #number - freq
+        pref_dictionary = {0:1}
+
+        total = 0
+        pref_number = 0
+
+        [0, 2, 1, 2, 4]
+        for i in range(len(nums)):
+            pref_number += nums[i]
+            #if 
+            if pref_number - k in pref_dictionary:
+                total += pref_dictionary[pref_number - k]
+
+    
+            pref_dictionary[pref_number] = pref_dictionary.get(pref_number, 0) + 1
+
+            
+           
+    
+ 
+       
+        return total
+
